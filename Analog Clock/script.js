b9d474de1663c67bc3321ee2a7fff3a3ce817740
Jagyan <92003595+j-a-g-y-a-n22@users.noreply.github.com>
2024-10-05 11:43:32 +0530

    Update script.js

--- Analog Clock/script.js
+++ Analog Clock/script.js
@@ -7,5 +7,8 @@ setInterval(() => {
     let color = "black"
     second.style.transform = `rotate(${6*cseconds}deg)`
     minute.style.transform = `rotate(${6*cminute}deg)`
-    hour.style.transform = `rotate(${30*chour}deg)`
+    hour.style.transform = `rotate(${30*chour+ 0.5*cminute}deg)`
+    hr.innerText = chour;
+    min.innerText = ": "+ cminute
+    sec.innerText = cseconds
 }, 1000);
